736e8385cc78567b5fd7b0cdfe5707c99430d090
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-08-10 13:49:09 -0400

    Use delivery local for testing
    Perhaps overwrought, but this is The Way Forward, so far as I can tell

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name 'build_cookbook'
+maintainer 'The Dark Sky Company, LLC'
+maintainer_email 'jeff@darksky.net'
+license 'all_rights'
+version '0.1.0'
+
+depends 'delivery-truck'
---
+++ .delivery/build_cookbook/recipes/functional.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: build_cookbook
+# Recipe:: functional
+#
+# Copyright (c) 2016 The Dark Sky Company, LLC, All Rights Reserved.
+include_recipe 'delivery-truck::functional'
