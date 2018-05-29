8a8ba803bd2e6c4492d8b7a9cab942237a78d3cb
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-05-29 12:29:41 -0400

    Bump to v1.3.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bruce@darksky.net'
 license          'Apache-2.0'
 description      'Builds and installs opencv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.1'
+version          '1.3.2'
 
 source_url 'https://github.com/darkskyapp/ds_opencv-cookbook'
 issues_url 'https://github.com/darkskyapp/ds_opencv-cookbook/issues'
