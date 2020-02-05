403bcec3df4b2e3b21d765ecfda650d4ec3c3cd5
Preston Mueller <preston@darksky.net>
2020-02-05 15:34:31 -0500

    Add support for OpenCV v2.x

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -19,30 +19,48 @@
 
 require 'spec_helper'
 
+shared_examples 'opencv' do
+  it 'converges successfully' do
+    expect { chef_run }.to_not raise_error
+  end
+
+  it 'downloads and unarchives OpenCV' do
+    expect(chef_run).to install_ark('opencv').with url: "https://github.com/opencv/opencv/archive/#{opencv_version}.zip"
+  end
+
+  it 'creates a release dir for OpenCV' do
+    expect(chef_run).to create_directory '/opt/opencv/release'
+  end
+
+  it 'builds & installs OpenCV from source' do
+    expect(chef_run).to run_execute 'cmake_opencv'
+
+    expect(chef_run).to run_execute('make_opencv').with(
+      creates: "/usr/local/lib/libopencv_core.so.#{opencv_version}"
+    )
+  end
+end
+
 describe 'opencv::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     platform 'ubuntu', '18.04'
 
     let(:opencv_version) { '3.4.9' }
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+    it_behaves_like 'opencv'
+  end
 
-    it 'downloads and unarchives OpenCV' do
-      expect(chef_run).to install_ark('opencv').with url: "https://github.com/opencv/opencv/archive/#{opencv_version}.zip"
-    end
+  context 'When installing OpenCV v2.x, on Ubuntu 18.04' do
+    platform 'ubuntu', '18.04'
 
-    it 'creates a release dir for OpenCV' do
-      expect(chef_run).to create_directory '/opt/opencv/release'
-    end
+    # README: Setting this as a local variable due to how RSpec handles scope
+    #         Avoids duplication in the following assignments
+    version = '2.4.13.4'
 
-    it 'builds & installs OpenCV from source' do
-      expect(chef_run).to run_execute 'cmake_opencv'
+    override_attributes['opencv']['opencv']['version'] = version
 
-      expect(chef_run).to run_execute('make_opencv').with(
-        creates: "/usr/local/lib/libopencv_core.so.#{opencv_version}"
-      )
-    end
+    let(:opencv_version) { version }
+
+    it_behaves_like 'opencv'
   end
 end
