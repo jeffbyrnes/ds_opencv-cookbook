a2c5d3663d49fe94b65c813872916aaf5422b802
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-02-08 15:03:14 -0500

    Update config & metadata files
    * Update chefignore per Chef 15 defaults
    * Autocorrect per current cookstyle
    * Fix headers to include license callout
        * The LICENSE has been Apache-2.0 since day 1, so the headers should
        reflect that.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'The Dark Sky Company, LLC'
 maintainer_email 'bruce@darksky.net'
 license          'Apache-2.0'
 description      'Builds and installs opencv'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.3.2'
 
 source_url 'https://github.com/darkskyapp/ds_opencv-cookbook'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,8 +1,21 @@
 #
-# Cookbook Name:: ds_opencv
+# Cookbook:: ds_opencv
 # Spec:: default
 #
-# Copyright (c) 2016 The Dark Sky Company, LLC, All Rights Reserved.
+# Copyright:: (c) 2016 The Dark Sky Company, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 require 'spec_helper'
 
@@ -33,7 +46,7 @@ describe 'ds_opencv::default' do
     it 'clones and checks out OpenCV v3.2.0' do
       expect(chef_run).to checkout_git(opencv_path).with(
         repository: 'https://github.com/opencv/opencv.git',
-        revision:   opencv_version.to_s
+        revision: opencv_version.to_s
       )
     end
 
@@ -46,13 +59,13 @@ describe 'ds_opencv::default' do
         command: 'cmake -D MAKE_BUILD_TYPE=RELEASE -D MAKE_INSTALL_PREFIX=/usr/local ' \
                  '-D BUILD_PERF_TESTS=OFF -D WITH_GTK=OFF -D WITH_FFMPEG=OFF ' \
                  '-D WITH_GSTREAMER=OFF -D WITH_CUDA=OFF ..',
-        cwd:     "#{opencv_path}/release",
+        cwd: "#{opencv_path}/release",
         creates: "#{opencv_path}/release/Makefile"
       )
 
       expect(chef_run).to run_execute('make_opencv').with(
         command: 'make -j4 && make install && ldconfig',
-        cwd:     "#{opencv_path}/release",
+        cwd: "#{opencv_path}/release",
         creates: "/usr/local/lib/libopencv_core.so.#{opencv_version}"
       )
     end
